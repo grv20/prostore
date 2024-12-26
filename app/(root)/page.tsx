e55700886c968c1368021e0a921ee5d1d899b4da
Gaurav <gauravarora0320@gmail.com>
2024-12-26 22:11:53 +0530

    sample product and product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,15 @@
-// export const metadata = { title: "Home" };
-
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 const Homepage = () => {
-  return <>Prostore</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
