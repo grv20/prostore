61949b69ac4c35ce64a9e82a40650a756e022e0c
Gaurav <gauravarora0320@gmail.com>
2025-04-03 12:59:44 +0530

    removed att to cart component

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -7,7 +7,7 @@ import {
   getLatestProducts,
 } from "@/lib/actions/product.actions";
 import { Badge } from "@/components/ui/badge";
-import AddToCart from "@/components/shared/product/add-to-cart";
+// import AddToCart from "@/components/shared/product/add-to-cart";
 
 export const revalidate = 120;
 
@@ -74,7 +74,7 @@ const ProductDetailsPage = async (props: {
                     <Badge variant="destructive">Unavailable</Badge>
                   )}
                 </div>
-                {product.stock > 0 && (
+                {/* {product.stock > 0 && (
                   <div className=" flex-center">
                     <AddToCart
                       item={{
@@ -87,7 +87,7 @@ const ProductDetailsPage = async (props: {
                       }}
                     />
                   </div>
-                )}
+                )} */}
               </CardContent>
             </Card>
           </div>
@@ -101,6 +101,10 @@ export default ProductDetailsPage;
 
 export async function generateStaticParams() {
   const products = await getLatestProducts();
+  console.log(
+    "Generating static params for products",
+    products.map((product) => ({ slug: product.slug }))
+  );
   return products.map((product) => ({
     slug: product.slug,
   }));
